bbff7e7756eae63d78663046a15f6c7bf9fecd50
ThongNguyen1213 <tommynguyenkmf@gmail.com>
2023-03-13 14:47:08 -0700

    first commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+  export const shazamCoreApi = createApi({
+      reducerPath: 'shazamCoreApi',
+      baseQuery: fetchBaseQuery({
+          baseUrl:'https://shazam-core.p.rapidapi.com/v1',
+          prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'e2f176c6b3mshd7c912b958c916ep1eb479jsnfd1213d90dbe');
+
+            return headers;
+          },
+      }),
+      endpoints: (builder) => ({
+          getTopCharts: builder.query({ query: () => '/charts/world'}),
+          getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}`}),
+          getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}`}),
+          getArtistDetails: builder.query({ query: ( artistId ) => `/artists/details?artist_id=${artistId}`}),
+      }),
+  });
+
+export const {  
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+} = shazamCoreApi;
